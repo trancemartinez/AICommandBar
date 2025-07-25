1d4e5149869773d7163cdd7101530ca242d9f77e
Chance Martinez <trancemartinez@icloud.com>
2025-07-25 12:28:06 -0500

    Initial Commit

---
+++ AICommandBarTests/AICommandBarTests.swift
@@ -0,0 +1,17 @@
+//
+//  AICommandBarTests.swift
+//  AICommandBarTests
+//
+//  Created by Chance Martinez on 7/25/25.
+//
+
+import Testing
+@testable import AICommandBar
+
+struct AICommandBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
